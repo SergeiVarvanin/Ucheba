0465c2880808b970bdc72f826e4c46c33da317b0
SergeiVarvanin <disidius@mail.ru>
2023-01-23 09:56:49 +0300

    HW
    HW

---
+++ HW2/ipython_log.py
@@ -0,0 +1,34 @@
+# IPython log file
+
+import sys
+sys.set_int_max_str_digits(50000)
+bin(1234567)
+#[Out]# '0b100101101011010000111'
+sys.set_int_max_str_digits(50000)
+0b00110011 | (1 << 7)
+#[Out]# 179
+a=1
+-10<a<100
+#[Out]# True
+42<<34
+#[Out]# 721554505728
+int(13.8)
+#[Out]# 13
+(13.888).__ceil__()
+#[Out]# 14
+(13.888).__floor__()
+#[Out]# 13
+round(13.888,2)
+#[Out]# 13.89
+int(13.888)
+#[Out]# 13
+int(13.888*2)
+#[Out]# 27
+int(13.888*1)
+#[Out]# 13
+int(13.888*1.5)
+#[Out]# 20
+int(13.888*1.1)
+#[Out]# 15
+int(13.888*1.01)
+#[Out]# 14
